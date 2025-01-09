010f5087d433650c57ab57ca8b24f3f5ec46cdde
Tommy R <drumming.dog101@gmail.com>
2025-01-09 02:48:49 -0500

    Should have fixed password problems

--- authenticate.js
+++ authenticate.js
@@ -1,9 +1,13 @@
 function authenticate (value) {
+    let procterPasscode;
     try {
-        let procterPasscode = document.getElementsByName("viewport")[0].id.split("|")[0]
-        // try {
-        //     procterPasscode = JSON.parse(localStorage.newPW);
-        // } catch (error) {}
+        procterPasscode = JSON.parse(localStorage.newPW);
+    } catch (error) {}
+
+    try {
+        if (procterPasscode == undefined) {
+            procterPasscode = document.getElementsByName("viewport")[0].id.split("|")[0]
+        }
         return value == procterPasscode || value == document.getElementsByName("viewport")[0].id.split("|")[1];
     } catch (error) {
         sessionStorage.isTest = JSON.stringify("false"); 
