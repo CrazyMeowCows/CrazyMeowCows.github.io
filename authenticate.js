ddf8834616c16cd33c93916b8479e896f41d1450
Tommy R <drumming.dog101@gmail.com>
2025-01-09 02:40:48 -0500

    Test

--- authenticate.js
+++ authenticate.js
@@ -1,9 +1,9 @@
 function authenticate (value) {
     try {
         let procterPasscode = document.getElementsByName("viewport")[0].id.split("|")[0]
-        try {
-            let newPW = JSON.parse(localStorage.newPW);
-        } catch (error) {}
+        // try {
+        //     procterPasscode = JSON.parse(localStorage.newPW);
+        // } catch (error) {}
         return value == procterPasscode || value == document.getElementsByName("viewport")[0].id.split("|")[1];
     } catch (error) {
         sessionStorage.isTest = JSON.stringify("false"); 
