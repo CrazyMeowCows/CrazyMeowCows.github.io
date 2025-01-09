7dca62b8fcca002a10a784cacf161c4c481938c7
Tommy R <drumming.dog101@gmail.com>
2025-01-09 01:24:14 -0500

    Added figure selection and 2nd figure

--- defaultSettings.js
+++ defaultSettings.js
@@ -4,3 +4,20 @@ const displayGrid = "true";
 const maxBrushSize = "5";
 const minBrushSize = "1";
 const defaultBrushPercentage = "100";
+
+function getFigure (selectedFigure, theta) {
+    switch(selectedFigure) {
+    case "1":
+        return figure1(theta);
+    case "2":
+        return figure2(theta);
+    }
+}
+
+function figure1 (t) {
+    return (1-1/6*(Math.sin(4*t+PI/2))-1/200*Math.pow(Math.cos(30*t), 2)*Math.sin(t)*Math.tan(t/2))/ROOT_PI
+}
+
+function figure2 (t) {
+    return 0.5;
+}
--- math.js
+++ math.js
@@ -1,8 +1,3 @@
-//Given a theta value, return the radius of the selected function--------------
-function calcRad(t) {
-    return (1-1/6*(Math.sin(4*t+PI/2))-1/200*Math.pow(Math.cos(30*t), 2)*Math.sin(t)*Math.tan(t/2))/ROOT_PI;
-}
-
 //Clamp number between minimum and maximum values------------------------------
 function clamp(x, min, max) {
     return Math.max(Math.min(x, max), min);
