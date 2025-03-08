4cf599a481c0f07fbcb8ac80c3dcd1551bc1fe13
Tommy Radabaugh <drumming.dog101@gmail.com>
2025-03-07 22:23:45 -0500

    Completely reworked touchListener

--- test-touchListener.js
+++ test-touchListener.js
@@ -18,71 +18,37 @@ figureCanvas.addEventListener('touchstart', (e) => e.preventDefault(), { passive
 figureCanvas.addEventListener("touchstart", e => {
     touches = correctTouches(e.touches);
     touch = touches[0];
-    let touchX = touch.pageX; 
-    let touchY = touch.pageY;
 
-    if (activePrompt) { //Any Prompt is Active
-        return;
-    }
+    if (activePrompt) return; //Any Prompt is Active
 
-    if (!DRAW_W_FINGER) { //Change input modes dependant on the drawWithFinger TODO: Clean up this code (Need access to iOS device)
-        if (touch.touchType == "stylus") { //Stylus
-            currentStroke = new PenStroke(touch.pageX, touch.pageY, brushColor);
-            strokes.push(currentStroke);
+    if ((DRAW_W_FINGER && touches.length == 1) || touch.touchType == "stylus") { //Single finger/stylus draw
+        currentStroke = new PenStroke(touch.pageX, touch.pageY, brushColor);
+        strokes.push(currentStroke);
 
-            drawCtx.fillStyle = brushColor;
-            drawCtx.strokeStyle = brushColor;
-            if (brushColor == ERASE_COLOR) {
-                drawCtx.globalCompositeOperation = "destination-out";
-                gridCtx.lineWidth = 1.5;
-                circle(touchX, touchY, BRUSH_SIZE, false, gridCtx);
-            } else {
-                drawCtx.globalCompositeOperation = "source-over";
-            }
-
-            circle(touch.pageX, touch.pageY, BRUSH_SIZE, true, drawCtx);
-        } 
-        else if (touches.length <= 1) { //Single Finger Pan
-            panX = touch.pageX;
-            panY = touch.pageY;
-        } 
-        else { //Multi finger zoom
-            zoomGestureDist = Math.hypot(touches[0].pageX - touches[1].pageX, touches[0].pageY - touches[1].pageY);
-            zoomX = (touches[0].pageX + touches[1].pageX)/2;
-            zoomY = (touches[0].pageY + touches[1].pageY)/2;
+        drawCtx.fillStyle = brushColor;
+        drawCtx.strokeStyle = brushColor;
+        if (brushColor == ERASE_COLOR) {
+            drawCtx.globalCompositeOperation = "destination-out";
+            gridCtx.lineWidth = 1.5;
+            circle(touch.pageX, touch.pageY, BRUSH_SIZE, false, gridCtx);
+        } else {
+            drawCtx.globalCompositeOperation = "source-over";
         }
-    } else {
-        if (touches.length == 1) { //Stylus or 1 Finger Touch
-            touchType = 1;
 
-            currentStroke = new PenStroke(touchX, touchY, brushColor);
-            strokes.push(currentStroke);
+        circle(touch.pageX, touch.pageY, BRUSH_SIZE, true, drawCtx);
+    } else if ((touches.length == 1 && !DRAW_W_FINGER)) { //Single Finger Pan
+        panX = touch.pageX;
+        panY = touch.pageY;
+    } else if (touches.length >= 2) { //2 Finger Pan/Zoom
+        let centerX = (touches[0].pageX + touches[1].pageX)/2;
+        let centerY = (touches[0].pageY + touches[1].pageY)/2;
 
-            drawCtx.fillStyle = brushColor;
-            drawCtx.strokeStyle = brushColor;
-            if (brushColor == ERASE_COLOR) {
-                drawCtx.globalCompositeOperation = "destination-out";
-                gridCtx.lineWidth = 1.5;
-                circle(touchX, touchY, BRUSH_SIZE, false, gridCtx);
-            } else {
-                drawCtx.globalCompositeOperation = "source-over";
-            }
+        panX = centerX;
+        panY = centerY;
 
-            circle(touchX, touchY, BRUSH_SIZE, true, drawCtx);
-        } 
-        else if (touches.length >= 2) { //2 Finger Pan/Zoom
-            touchType = 2;
-
-            let centerX = (touches[0].pageX + touches[1].pageX)/2;
-            let centerY = (touches[0].pageY + touches[1].pageY)/2;
-
-            panX = centerX;
-            panY = centerY;
-
-            zoomGestureDist = Math.hypot(touches[0].pageX - touches[1].pageX, touches[0].pageY - touches[1].pageY);
-            zoomX = centerX;
-            zoomY = centerY;
-        }
+        zoomGestureDist = Math.hypot(touches[0].pageX - touches[1].pageX, touches[0].pageY - touches[1].pageY);
+        zoomX = centerX;
+        zoomY = centerY;
     }
 })
 
@@ -93,86 +59,43 @@ figureCanvas.addEventListener("touchmove", e => {
     let touchX = touch.pageX; 
     let touchY = touch.pageY;
 
-    if (activePrompt) { //Any Prompt is Active
-        return;
-    }
-    if (DRAW_W_FINGER) { //Change input modes dependant on the drawWithFinger TODO: Clean up this code (Need access to iOS device)
-        if (touches.length <= 1 && currentStroke == undefined) { //Trying to draw but no currentStroke
-            return;
-        }
-        if (touches.length == 1 && currentStroke != undefined && touchType == 1) { //Stylus
-            gridCtxRedraw();
+    if (activePrompt) return; //Any Prompt is Active
+
+    if ((DRAW_W_FINGER && touches.length == 1) || touch.touchType == "stylus") { //Single finger/stylus draw
+        if (currentStroke == undefined) return; //Trying to draw but no currentStroke
             
-            line(lastStrokeX, lastStrokeY, touchX, touchY, BRUSH_SIZE*2, drawCtx);
-            extendCurrentStroke(touchX, touchY);
-            
-            if (brushColor == ERASE_COLOR) {
-                gridCtx.lineWidth = 1.5;
-                circle(touchX, touchY, BRUSH_SIZE, false, gridCtx);
-            }
-            return;
+        gridCtxRedraw();
+        
+        line(lastStrokeX, lastStrokeY, touchX, touchY, BRUSH_SIZE*2, drawCtx);
+        extendCurrentStroke(touchX, touchY);
+        
+        if (brushColor == ERASE_COLOR) {
+            gridCtx.lineWidth = 1.5;
+            circle(touchX, touchY, BRUSH_SIZE, false, gridCtx);
         }
-        if (touches.length >= 2 && touchType == 2) { //2 Finger Pan/Zoom
-            let centerX = (touches[0].pageX + touches[1].pageX)/2;
-            let centerY = (touches[0].pageY + touches[1].pageY)/2;
+    } else if ((touches.length == 1 && !DRAW_W_FINGER)) { //Single Finger Pan
+        offsetX += touchX-panX;
+        offsetY += touchY-panY;
 
-            if (lastZoomed) {
-                panX = centerX;
-                panY = centerY;
-                lastZoomed = false
-            }
+        panX = touchX;
+        panY = touchY;
 
-            offsetX += centerX-panX;
-            offsetY += centerY-panY;
+        mainRedraw();
+    } else if (touches.length >= 2) { //2 Finger Pan/Zoom
+        let centerX = (touches[0].pageX + touches[1].pageX)/2;
+        let centerY = (touches[0].pageY + touches[1].pageY)/2;
 
-            panX = centerX;
-            panY = centerY;
+        offsetX += centerX-panX;
+        offsetY += centerY-panY;
 
-            let newZoomDistance = Math.hypot(touches[0].pageX-touches[1].pageX, touches[0].pageY-touches[1].pageY);
-            zoom = clamp(zoom - (zoomGestureDist-newZoomDistance)*ZOOM_SENS*Math.abs(zoom), 1, MAX_ZOOM);
-            zoomGestureDist = newZoomDistance;
+        panX = centerX;
+        panY = centerY;
 
-            mainRedraw();
-        }
-    } else {
-        if (touch.touchType == "stylus" && currentStroke == undefined) { //Trying to draw but no currentStroke
-            return;
-        }
-        if (touch.touchType == "stylus" && currentStroke != undefined) { //Stylus
-            gridCtxRedraw();
-            
-            line(lastStrokeX, lastStrokeY, touchX, touchY, BRUSH_SIZE*2, drawCtx);
-            extendCurrentStroke(touchX, touchY);
-            
-            if (brushColor == ERASE_COLOR) {
-                gridCtx.lineWidth = 1.5;
-                circle(touchX, touchY, BRUSH_SIZE, false, gridCtx);
-            }
-            return;
-        }
-        if (touches.length <= 1) { //Single Finger Pan
-            if (lastZoomed) {
-                panX = touchX;
-                panY = touchY;
-                lastZoomed = false
-            }
+        let newZoomDistance = Math.hypot(touches[0].pageX-touches[1].pageX, touches[0].pageY-touches[1].pageY);
+        zoom = clamp(zoom - (zoomGestureDist-newZoomDistance)*ZOOM_SENS*Math.abs(zoom), 1, MAX_ZOOM);
+        zoomGestureDist = newZoomDistance;
 
-            offsetX += touchX-panX;
-            offsetY += touchY-panY;
-
-            panX = touchX;
-            panY = touchY;
-
-            mainRedraw();
-        } 
-        else if (touches.length > 1) { //Multi finger zoom
-            let newZoomDistance = Math.hypot(touches[0].pageX-touches[1].pageX, touches[0].pageY-touches[1].pageY);
-            zoom = clamp(zoom - (zoomGestureDist-newZoomDistance)*ZOOM_SENS*Math.abs(zoom), 1, MAX_ZOOM);
-            zoomGestureDist = newZoomDistance;
-
-            lastZoomed = true;
-            mainRedraw();
-        }
+        mainRedraw();
     }
 })
 
