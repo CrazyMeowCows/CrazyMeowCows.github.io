86817c1b328d96d0f12838366c28eb0f019c3364
Tommy Radabaugh <77948158+CrazyMeowCows@users.noreply.github.com>
2025-05-18 17:35:41 -0400

    Update navBar.js

--- navBar.js
+++ navBar.js
@@ -1,5 +1,5 @@
 function changePage(destination) {
     if (destination == "encoder") destination = "index";
-    alert(destination);
-    location.href = destination + ".html"
+    console.log(destination);
+    //location.href = destination + ".html"
 }
